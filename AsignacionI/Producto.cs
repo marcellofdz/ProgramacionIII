f333f3eeef05e9e50a604ee5196b1a7d597098c1
marcellofdz <marcellofernandez98@gmail.com>
2021-02-08 19:58:51 -0400

    codigo funcional

---
+++ AsignacionI/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace AsignacionI { 
+public class Producto
+{
+		public int ID { get; set; }
+		
+		public string Detalle { get; set; }
+
+		public bool Existe { get; set; }
+
+}
+}
---
+++ AsignacionI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AsignacionI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        
+                Producto item = new Producto();
+                Console.Write("Put your ID: ");
+                item.ID = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Name: ");
+                item.Detalle = Console.ReadLine();
+                item.Existe = true;
+
+                Console.WriteLine($" Datos: {item.ID}, {item.Detalle}, {item.Existe}");
+            
+
+            Console.Read();
+
+        }
+    }
+}
