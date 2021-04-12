19075c29d136fda876d2c77f9ae44976db955db8
marcellofdz <marcellofernandez98@gmail.com>
2021-04-12 19:14:06 -0400

    Adicionando proyecto nuevos e inicializando variables

---
+++ RegistroEmpleados/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroEmpleados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nombres;
+            string Apellidos;
+            string Cedula;
+            string Matricula;
+            string NombreCentro;
+            string Direccion;
+            string Ciudad;
+            string Sexo;
+            string FechaNacimiento;
+            string Puesto;
+            string Sueldo;
+            string Estado;
+        }
+    }
+}
