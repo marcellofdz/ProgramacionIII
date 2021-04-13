4aa13cc942f06d1cd2db7c081b7052c1e6adf6bd
marcellofdz <marcellofernandez98@gmail.com>
2021-04-12 21:12:04 -0400

    Creacion de tablas en base de datos

--- RegistroEmpleados/Program.cs
+++ RegistroEmpleados/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace RegistroEmpleados
 {
@@ -10,18 +13,71 @@ namespace RegistroEmpleados
     {
         static void Main(string[] args)
         {
-            string Nombres;
-            string Apellidos;
-            string Cedula;
-            string Matricula;
-            string NombreCentro;
-            string Direccion;
-            string Ciudad;
-            string Sexo;
-            string FechaNacimiento;
-            string Puesto;
-            string Sueldo;
-            string Estado;
+        string Matricula, Direccion, Ciudad, Estado, MatriculaCentro, Cedula, Nombres, Apellidos, Sexo, FechaNacimiento, Puesto, Sueldo;
+
+            Int32 CantidadRegistros;
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["connection"].ConnectionString);
+            connection.Open();
+            Console.WriteLine(connection.State);
+
+            string salir = "no";
+            SqlTransaction tran = null;
+            SqlCommand cmd = null;
+
+            Console.WriteLine();
+
+            while (salir.ToLower() == "no")
+            {
+
+                Console.Write("Ingrese el nombre: ");
+                Nombres = Console.ReadLine();
+
+                Console.Write("Ingrese los apellidos: ");
+                Apellidos = Console.ReadLine();
+
+                Console.Write("Ingrese el sexo: ");
+                Sexo = Console.ReadLine();
+
+                Console.Write("Ingrese la cédula: ");
+                Cedula = Console.ReadLine();
+
+                Console.Write("Ingrese la fecha de nacimiento: ");
+                FechaNacimiento = Console.ReadLine();
+
+                Console.Write("Ingrese la Matricula: ");
+                Matricula = Console.ReadLine();
+
+                Console.Write("Ingrese la Matricula del centro: ");
+                MatriculaCentro = Console.ReadLine();
+
+                Console.Write("Puesto designado: ");
+                Puesto = Console.ReadLine();
+
+                Console.Write("Sueldo designado: ");
+                Sueldo = Console.ReadLine();
+
+                Console.Write("Estado: ");
+                Estado = Console.ReadLine();
+
+
+                try
+                {
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                    Console.WriteLine("Ocurrió un error, contacta con el palomo de soporte.");
+                }
+
+                Console.Write("Desea salir? Si/no : ");
+                salir = Console.ReadLine();
+            }
+
+
+            Console.ReadKey();
+
         }
     }
 }
